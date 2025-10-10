b4e78e448178a4d946bfc0673ca10fa144c89f6d
Subhankar <subhankardhal637@gmail.com>
2025-10-10 21:46:19 +0530

    Switch Statement

---
+++ JavaScript/Day_3/file_4.js
@@ -0,0 +1,58 @@
+// Switch Statement 
+
+// let day = 0;
+
+// if (day===0){
+//   console.log("Today is Sunday");
+// }
+// else if(day === 1){
+//   console.log("Today is Monday");
+// }
+// else if (day === 2){
+//   console.log("Today is Tuesday");
+// }
+// else if (day === 3) {
+//   console.log("Today is Wednessday");
+// }
+// else if (day === 4) {
+//   console.log("Today is Thursday");
+// }
+// else if (day === 5) {
+//   console.log("Today is Friday");
+// }
+// else if (day === 6) {
+//   console.log("Today is Saturday");
+// }
+// else{
+//   console.log("Invalid Day !");
+// }
+
+// Les's code using Switch Statement
+
+let day = 0;
+
+switch(day){
+  case 0:
+    console.log("Today is Sunday");
+    break;
+  case 1:
+    console.log("Today is Monday");
+    break;
+  case 2:
+    console.log("Today is Tuesday");
+    break;
+  case 3:
+    console.log("Today is Wednessday");
+    break;
+  case 4:
+    console.log("Today is Thursday");
+    break;
+  case 5:
+    console.log("Today is Friday");
+    break;
+  case 6:
+    console.log("Today is Saturday");
+    break;
+  default:
+    console.log("Invaild Day !"); 
+}
